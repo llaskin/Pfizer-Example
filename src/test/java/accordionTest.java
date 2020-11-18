b56f29ed1c37a8de9515a5b609b5ab53f4536cd6
Leo Laskin <llaskin@gmail.com>
2020-11-18 10:32:45 -0500

    updating cross browser tests to run a bit better

--- src/test/java/accordionTest.java
+++ src/test/java/accordionTest.java
@@ -3,10 +3,7 @@
 //Step 3: Click a link on iBrance.com ("For Newly Diagnosed")
 //Step 4: Load the link
 //Step 5: Validate that the For Newly Diagnosed page is as expected.
-import com.applitools.eyes.BatchInfo;
-import com.applitools.eyes.EyesRunner;
-import com.applitools.eyes.RectangleSize;
-import com.applitools.eyes.StdoutLogHandler;
+import com.applitools.eyes.*;
 import com.applitools.eyes.selenium.*;
 import com.applitools.eyes.selenium.fluent.Target;
 import com.applitools.eyes.visualgrid.model.DeviceName;
@@ -18,6 +15,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 import javax.swing.*;
@@ -38,8 +36,8 @@ public class accordionTest {
         // Create Eyes object with the runner, meaning it'll be a Visual Grid eyes.
         Eyes eyes = new Eyes(runner);
         // Set API key
-        eyes.setApiKey("Y7bRjY1ZkKKR9a4F1wCMNlBVyr1y6a23ckJo9I7ihQM110");
-//        eyes.setLogHandler(new StdoutLogHandler(true));
+        eyes.setApiKey(System.getenv("APPLITOOLS_API_KEY"));
+        eyes.setLogHandler(new StdoutLogHandler(true));
         // Create SeleniumConfiguration.
         Configuration sconf = new Configuration();
         // Set the AUT name
@@ -52,27 +50,15 @@ public class accordionTest {
         // Add Chrome browsers with different Viewports
         sconf.addBrowser(1440, 820, BrowserType.CHROME);
         sconf.addBrowser(700, 500, BrowserType.CHROME);
-        sconf.addBrowser(700, 500, BrowserType.CHROME_ONE_VERSION_BACK);
-        sconf.addBrowser(700, 500, BrowserType.CHROME_TWO_VERSIONS_BACK);
         // Add Firefox browser with different Viewports
         sconf.addBrowser(1440, 820, BrowserType.FIREFOX);
         sconf.addBrowser(1600, 1200, BrowserType.FIREFOX);
         //Add IE 10 browser with different viewports
         sconf.addBrowser(1024, 768, BrowserType.IE_10);
-        sconf.addBrowser(1600, 1200, BrowserType.IE_10);
         //Add IE 10 browser wiath different viewports
-        sconf.addBrowser(700, 500, BrowserType.IE_11);
         sconf.addBrowser(1000, 900, BrowserType.IE_11);
-        sconf.addBrowser(1000, 900, BrowserType.EDGE_CHROMIUM_ONE_VERSION_BACK);
         sconf.addBrowser(1000, 900, BrowserType.EDGE_CHROMIUM);
-        sconf.addBrowser(1000, 900, BrowserType.EDGE_LEGACY);
-        sconf.addBrowser(1000, 900, BrowserType.EDGE);
-        sconf.addBrowser(1000, 900, BrowserType.SAFARI_ONE_VERSION_BACK);
-        sconf.addBrowser(1000, 900, BrowserType.SAFARI_TWO_VERSIONS_BACK);
-        sconf.addBrowser(1000, 900, BrowserType.SAFARI_TWO_VERSIONS_BACK);
-        // Add iPhone device emulation
-        sconf.addDeviceEmulation(DeviceName.iPhone_X);
-        sconf.addDeviceEmulation(DeviceName.iPhone_6_7_8);
+        sconf.addBrowser(1000, 900, BrowserType.SAFARI);
         // Set the configuration object to eyes
         eyes.setConfiguration(sconf);
         return eyes;
@@ -80,6 +66,8 @@ public class accordionTest {
     @Test()
     public void loadiBrance() throws InterruptedException {
         int i;
+        WebElement currentAccordion = null;
+
         // Create a runner with concurrency of 10
         VisualGridRunner runner = new VisualGridRunner(10);
         //Initialize Eyes with Visual Grid Runner
@@ -89,7 +77,6 @@ public class accordionTest {
         options.addArguments("--disable-gpu");
         ChromeDriver driver = new ChromeDriver(options);
 
-        eyes.setApiKey(System.getenv("APPLITOOLS_API_KEY"));
         eyes.setForceFullPageScreenshot(true);
         eyes.setStitchMode(StitchMode.CSS);
 
@@ -97,18 +84,33 @@ public class accordionTest {
 
         driver.get(url);
         lazyLoadPage(driver);
-//        eyes.check("Full Screen popup", Target.window().fully());
-        List<WebElement> accordion = driver.findElements(By.cssSelector(".accordion-row"));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", accordion.get(0));
 
+        //Thread.sleep to make sure Tablets link closes
+        Thread.sleep(5000);
 
-        for(i = 0; i <= accordion.size(); i++)
+        List<WebElement> accordion = driver.findElements(By.cssSelector(".accordion-row .accordion-header"));
+
+        Actions actions = new Actions(driver);
+        System.out.println("Accordion size = " + accordion.size());
+
+        for(i = 1; i < accordion.size(); i++)
         {
-            accordion.get(i).findElement(By.cssSelector(".accordion-header")).click();
-            eyes.check("Accordion " + i, Target.window().fully());
+            System.out.println(i);
+            currentAccordion = accordion.get(i);
+
+            actions.moveToElement(currentAccordion).click().perform();
+            eyes.check("Accordion open " + i, Target.window().fully());
+            System.out.println("Closing");
+            actions.moveToElement(currentAccordion).click().perform();
+            eyes.check("Accordion closed " + i, Target.window().fully());
 
         }
+
         eyes.closeAsync();
+
+        TestResultsSummary allTestResults = runner.getAllTestResults(false);
+        System.out.println(allTestResults);
+
         driver.quit();
     }
 }
--- src/test/java/crossBrowserTest.java
+++ src/test/java/crossBrowserTest.java
@@ -3,10 +3,7 @@
 //Step 3: Click a link on iBrance.com ("For Newly Diagnosed")
 //Step 4: Load the link
 //Step 5: Validate that the For Newly Diagnosed page is as expected.
-import com.applitools.eyes.BatchInfo;
-import com.applitools.eyes.EyesRunner;
-import com.applitools.eyes.RectangleSize;
-import com.applitools.eyes.StdoutLogHandler;
+import com.applitools.eyes.*;
 import com.applitools.eyes.selenium.*;
 import com.applitools.eyes.selenium.fluent.Target;
 import com.applitools.eyes.visualgrid.model.DeviceName;
@@ -28,7 +25,8 @@ public class crossBrowserTest {
         JavascriptExecutor js = (JavascriptExecutor) driver;
         Long height = (Long) js.executeScript("return document.body.scrollHeight;");
         for (int i = 0; i < height / 100; i++)
-            js.executeScript("window.scrollBy(0,100)");
+            js.executeScript("window.scrollBy(0,500)");
+        js.executeScript("window.scrollTo(0,0)");
     }
 
     public static Eyes initializeEyes(VisualGridRunner runner) {
@@ -49,24 +47,16 @@ public class crossBrowserTest {
         // Add Chrome browsers with different Viewports
         sconf.addBrowser(1440, 820, BrowserType.CHROME);
         sconf.addBrowser(700, 500, BrowserType.CHROME);
-        sconf.addBrowser(700, 500, BrowserType.CHROME_ONE_VERSION_BACK);
-        sconf.addBrowser(700, 500, BrowserType.CHROME_TWO_VERSIONS_BACK);
         // Add Firefox browser with different Viewports
         sconf.addBrowser(1440, 820, BrowserType.FIREFOX);
         sconf.addBrowser(1600, 1200, BrowserType.FIREFOX);
         //Add IE 10 browser with different viewports
         sconf.addBrowser(1024, 768, BrowserType.IE_10);
-        sconf.addBrowser(1600, 1200, BrowserType.IE_10);
         //Add IE 10 browser wiath different viewports
         sconf.addBrowser(700, 500, BrowserType.IE_11);
-        sconf.addBrowser(1000, 900, BrowserType.IE_11);
-        sconf.addBrowser(1000, 900, BrowserType.EDGE_CHROMIUM_ONE_VERSION_BACK);
         sconf.addBrowser(1000, 900, BrowserType.EDGE_CHROMIUM);
         sconf.addBrowser(1000, 900, BrowserType.EDGE_LEGACY);
-        sconf.addBrowser(1000, 900, BrowserType.EDGE);
-        sconf.addBrowser(1000, 900, BrowserType.SAFARI_ONE_VERSION_BACK);
-        sconf.addBrowser(1000, 900, BrowserType.SAFARI_TWO_VERSIONS_BACK);
-        sconf.addBrowser(1000, 900, BrowserType.SAFARI_TWO_VERSIONS_BACK);
+        sconf.addBrowser(1000, 900, BrowserType.SAFARI);
         // Add iPhone device emulation
         sconf.addDeviceEmulation(DeviceName.iPhone_X);
         sconf.addDeviceEmulation(DeviceName.iPhone_6_7_8);
@@ -85,16 +75,22 @@ public class crossBrowserTest {
         options.addArguments("--disable-gpu");
         ChromeDriver driver = new ChromeDriver(options);
 
-        eyes.setApiKey("Y7bRjY1ZkKKR9a4F1wCMNlBVyr1y6a23ckJo9I7ihQM110");
         eyes.setForceFullPageScreenshot(true);
         eyes.setStitchMode(StitchMode.CSS);
 //        eyes.setLogHandler(new StdoutLogHandler(true));
 
-        eyes.open(driver, "iBrance", "iBrance Cross Browser", new RectangleSize(1200, 600));
+        eyes.open(driver);
 
         driver.get(url);
         lazyLoadPage(driver);
+
         eyes.check("Full Screen popup", Target.window().fully());
+
+        eyes.closeAsync();
+        TestResultsSummary allTestResults = runner.getAllTestResults(false);
+        System.out.println(allTestResults);
+
         driver.quit();
+
     }
 }
